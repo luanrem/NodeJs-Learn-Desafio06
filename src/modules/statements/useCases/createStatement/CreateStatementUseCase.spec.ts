dd2e322e25c9a2101a70d3b66b402d133689f974
luanrem <luanrem@gmail.com>
2022-11-20 23:14:29 -0300

    Finished CreateStatementUseCase test

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -1,8 +1,98 @@
+import { CreateStatementUseCase } from './CreateStatementUseCase';
+import { InMemoryStatementsRepository } from './../../repositories/in-memory/InMemoryStatementsRepository';
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../../../users/useCases/createUser/CreateUserUseCase";
+import { ICreateUserDTO } from "../../../users/useCases/createUser/ICreateUserDTO";
+import { ICreateStatementDTO } from './ICreateStatementDTO';
+import { AppError } from '../../../../shared/errors/AppError';
 
+enum OperationType {
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
+}
+
+let statementsRepositoryInMemory: InMemoryStatementsRepository
+let usersRepositoryInMemory: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+let createStatementUseCase: CreateStatementUseCase
 
 describe("Create Statement", () => {
 
+  beforeEach(() => {
+    statementsRepositoryInMemory = new InMemoryStatementsRepository()
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    createStatementUseCase = new CreateStatementUseCase(usersRepositoryInMemory, statementsRepositoryInMemory)
+  })
+
   it("should be able to create a new statement", async () => {
-    const statement = {}
+    const newUser: ICreateUserDTO = {
+      name: "Marc Cummings",
+      email: "fum@zoafo.gg",
+      password: "1234",
+    };
+
+    const user = await createUserUseCase.execute(newUser);
+
+    const type = "deposit" as OperationType
+
+    const statement: ICreateStatementDTO = {
+      user_id: user.id,
+      amount: 20,
+      type,
+      description: "description"
+    }
+
+    const statementCreated = await createStatementUseCase.execute(statement);
+
+    expect(statementCreated).toHaveProperty("id")
+    expect(statementCreated).toHaveProperty("user_id")
+    expect(statementCreated).toHaveProperty("amount")
+    expect(statementCreated).toHaveProperty("type")
+    expect(statementCreated).toHaveProperty("description")
+  })
+
+  it("should not be able to create a statement with a inexistent user", async () => {
+    const type = "deposit" as OperationType
+
+    const statement: ICreateStatementDTO = {
+      user_id: "3818995447",
+      amount: 20,
+      type,
+      description: "description"
+    }
+
+    expect(async () => {
+      await createStatementUseCase.execute(statement);
+    }).rejects.toBeInstanceOf(AppError);
+
+  })
+
+  it("should not be able to withdraw if balance is less than amount", async () => {
+    const newUser: ICreateUserDTO = {
+      name: "Lulu Wade",
+      email: "ro@hahamti.pn",
+      password: "2870421770",
+    };
+
+    const user = await createUserUseCase.execute(newUser);
+
+    await createStatementUseCase.execute({
+      user_id: user.id,
+      amount: 20,
+      type: "deposit" as OperationType,
+      description: "description"
+    });
+
+
+    expect(async () => {
+      await createStatementUseCase.execute({
+        user_id: user.id,
+        amount: 40,
+        type: "withdraw" as OperationType,
+        description: "description"
+      });
+    }).rejects.toBeInstanceOf(AppError);
+
   })
 })
--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.ts
@@ -1,5 +1,5 @@
+import "reflect-metadata"
 import { inject, injectable } from "tsyringe";
-
 import { IUsersRepository } from "../../../users/repositories/IUsersRepository";
 import { IStatementsRepository } from "../../repositories/IStatementsRepository";
 import { CreateStatementError } from "./CreateStatementError";
@@ -13,16 +13,16 @@ export class CreateStatementUseCase {
 
     @inject('StatementsRepository')
     private statementsRepository: IStatementsRepository
-  ) {}
+  ) { }
 
   async execute({ user_id, type, amount, description }: ICreateStatementDTO) {
     const user = await this.usersRepository.findById(user_id);
 
-    if(!user) {
+    if (!user) {
       throw new CreateStatementError.UserNotFound();
     }
 
-    if(type === 'withdraw') {
+    if (type === 'withdraw') {
       const { balance } = await this.statementsRepository.getUserBalance({ user_id });
 
       if (balance < amount) {
